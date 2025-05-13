5ae7ba6d6c4799969cb9ec938e12b85e62d5b017
truong288 <truongtuong.k20@gmail.com>
2025-05-13 10:18:32 +0700

    Update stay_alive.py

--- stay_alive.py
+++ stay_alive.py
@@ -1,24 +1,6 @@
-from flask import Flask, render_template
-from threading import Thread
-import os
-
+from flask import Flask
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
-
-  replit_url = f'https://{os.environ["REPL_SLUG"]}.{os.environ["REPL_OWNER"]}.replit.co'
-  return f"Alive! Replit Project URL: {replit_url}"
-
-
-def run():
-  app.run(host='0.0.0.0', port=8080)
-
-
-def keep_alive():
-  t = Thread(target=run)
-  t.start()
-
-
-keep_alive()
+def home():
+    return "Alive"
